52b899a43df3ea134fd3eff85c337878f767dbd3
ggrubbs <ggrubbs@546ca710-db30-0410-8887-d9dd8e66aff0>
2009-07-12 20:59:34 +0000

    Revved to version 1.2.1 after tagging version 1.2
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-links/trunk@13428 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-links.php
+++ cf-links.php
@@ -3,7 +3,7 @@
 Plugin Name: CF Links
 Plugin URI: http://crowdfavorite.com
 Description: Advanced options for adding links
-Version: 1.2
+Version: 1.2.1
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */

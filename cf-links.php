12762bb821743b9ea64171efde27785d74999e9e
Sean Klein <sean+git@crowdfavorite.com>
2012-02-10 14:58:37 -0700

    updating the version number and changelog

--- cf-links.php
+++ cf-links.php
@@ -3,7 +3,7 @@
 Plugin Name: CF Links
 Plugin URI: http://crowdfavorite.com
 Description: Advanced tool for adding collections of links, including pages, posts, and external URLs.
-Version: 1.4
+Version: 1.4.1
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */
